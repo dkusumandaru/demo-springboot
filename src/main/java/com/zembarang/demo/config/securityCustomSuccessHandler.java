ec3e97244d07f1374f02601a4d8c3c43a9e85c32
dkusumandaru <darmakusumandaru@gmail.com>
2021-06-26 17:10:20 +0700

    update custom request handler

---
+++ src/main/java/com/zembarang/demo/config/securityCustomSuccessHandler.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zembarang.demo.config;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author dragon
+ */
+@Component
+public class securityCustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+    
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    
+    @Override
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws 
+            IOException{
+        
+        String targetUrl = determineTargetUrl(authentication);
+        if (response.isCommitted()) {
+            System.out.println("Can't Rediect");
+            return;
+        }
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+        
+    }
+   
+    protected String determineTargetUrl(Authentication authentication){
+        System.out.println("DETERMINE");
+        String url = "";
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        List<String> roles = new ArrayList<String>();
+        
+        for (GrantedAuthority a : authorities) {
+            roles.add(a.getAuthority());
+        }
+        
+        System.out.println("Role determineTargetUrl " + roles);
+        String role = roles.toString();
+        
+        System.out.println("Role : " + role);
+        
+        if (role.equals("[ROLE_admin]")) {
+            url = "product/category";
+        } else if(role.equals("[ROLE_user]")){
+            url = "product";
+        } else{
+            url = "doctor";
+        }
+        
+        return url;
+    }
+    
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy){
+        this.redirectStrategy = redirectStrategy;
+    }
+    
+    protected RedirectStrategy getRedirectStrategy(){
+        return redirectStrategy;
+    }
+   }
--- src/main/java/com/zembarang/demo/entity/NewUserDetails.java
+++ src/main/java/com/zembarang/demo/entity/NewUserDetails.java
@@ -5,65 +5,101 @@
  */
 package com.zembarang.demo.entity;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+//import java.util.Arrays;
+//import java.util.Collection;
+//import java.util.List;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  *
  * @author D
  */
-public class NewUserDetails implements UserDetails{
+public class NewUserDetails{
     
     private String usernameUser;
     private String passwordUser;
-    private List<GrantedAuthority> authorities;
+//    private List<GrantedAuthority> authorities;
+    private String roleUser;
+    private String idUser;
 
-    public NewUserDetails(String usernameUser, String passwordUser, String roleUser) {
+    public NewUserDetails(String usernameUser, String passwordUser, String roleUser, String idUser) {
         this.usernameUser = usernameUser;
         this.passwordUser = passwordUser;
-        this.authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_"+roleUser));
+//        this.authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_"+roleUser));
+        this.roleUser = roleUser;
+        this.idUser = idUser;
     }
 
+ 
+   
     
-    
-    
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return authorities;
+//    }
 
-    @Override
-    public String getPassword() {
-        return passwordUser;
-    }
+//    @Override
+//    public String getPassword() {
+//        return passwordUser;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return usernameUser;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//    
 
-    @Override
-    public String getUsername() {
+    public String getUsernameUser() {
         return usernameUser;
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
+    public void setUsernameUser(String usernameUser) {
+        this.usernameUser = usernameUser;
     }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
+    public String getPasswordUser() {
+        return passwordUser;
     }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
+    public void setPasswordUser(String passwordUser) {
+        this.passwordUser = passwordUser;
     }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
+    public String getRoleUser() {
+        return roleUser;
     }
-    
+
+    public void setRoleUser(String roleUser) {
+        this.roleUser = roleUser;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setId(String idUser) {
+        this.idUser = idUser;
+    }
 }
